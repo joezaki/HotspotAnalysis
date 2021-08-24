f3b46de94b20b152b0d0d9944f886e590203ffaf
joezaki <joe.zaki@icahn.mssm.edu>
2021-08-24 18:49:12 -0400

    Fix ROI functions and their implementation in the notebook for when the ROI is loaded rather than drawn

--- HotspotAnalysis/ROIFunctions.py
+++ HotspotAnalysis/ROIFunctions.py
@@ -55,13 +55,13 @@ def make_ROI(img, DAPI, region, vertices):
     plt.imshow(maskedImage, cmap='gist_gray')
     plt.title("Processed Image; Ready for Analysis")
     
-    return ROI_masks, mask, croppedImg, maskedImage, fig1, fig2
+    return mask, croppedImg, maskedImage, fig1, fig2
 
 
 def load_ROI(img, DAPI, roiPath):
     
     # load mask using the roiPath
-    mask = cv2.imread('./ROIs/' + ROIs[i])[:,:,0].astype(bool)
+    mask = cv2.imread(roiPath)[:,:,0].astype(bool)
     
     # create mask as in make_ROI function
     maskedImage = np.ma.array(img, mask = ~mask)
@@ -72,15 +72,15 @@ def load_ROI(img, DAPI, roiPath):
     ymin = next((i for i, x in enumerate(yrange) if x), None)
     ymax = len(yrange) - next((i for i, x in enumerate(np.flip(yrange, axis=0)) if x), None)
     croppedImg = img[xmin:xmax,ymin:ymax]
-    mask = mask[xmin:xmax,ymin:ymax]
+    new_mask = mask[xmin:xmax,ymin:ymax]
     maskedImage = maskedImage[xmin:xmax,ymin:ymax]
     
     fig1 = plt.figure(figsize=(25,15))
-    plt.imshow(img*ROI_masks['ROI'], cmap='pink')
+    plt.imshow(img*mask, cmap='pink')
     plt.imshow(img, cmap='gist_gray', alpha=0.6)
-    plt.plot([xs[-1],xs[0]],[ys[-1],ys[0]], 'ro-', alpha=0.2)
-    for i in range(0, len(xs)):
-        plt.plot(xs[i:i+2], ys[i:i+2], 'ro-', alpha=0.2)
+#     plt.plot([xs[-1],xs[0]],[ys[-1],ys[0]], 'ro-', alpha=0.2)
+#     for i in range(0, len(xs)):
+#         plt.plot(xs[i:i+2], ys[i:i+2], 'ro-', alpha=0.2)
     
     fig2 = plt.figure(figsize=(30,20))
     plt.subplot(121)
@@ -89,7 +89,7 @@ def load_ROI(img, DAPI, roiPath):
     plt.imshow(maskedImage, cmap='gist_gray')
     plt.title("Processed Image; Ready for Analysis")
     
-    return ROI_masks, mask, croppedImg, maskedImage, fig1, fig2
+    return new_mask, croppedImg, maskedImage, fig1, fig2
 
 
 # Define function that allows you to draw your ROI
